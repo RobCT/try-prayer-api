f02ada0a321d0d895a601fbb61b8268bb02733a4
robtay <robins.mail@ntlworld.com>
2015-04-25 20:39:23 +0100

    Correct validator by checking for instance of tested properties

--- app/validators/eventend_greaterthan_eventstart_validator.rb
+++ app/validators/eventend_greaterthan_eventstart_validator.rb
@@ -1,7 +1,9 @@
  class EventendGreaterthanEventstartValidator < ActiveModel::Validator
   def validate(record)
-    unless record.eventend > record.eventstart
-      record.errors[:eventend] << 'Event end must be after event start!'
+    if record.eventend && record.eventstart
+      unless record.eventend > record.eventstart
+        record.errors[:eventend] << 'Event end must be after event start!'
+      end
     end
   end
 end
